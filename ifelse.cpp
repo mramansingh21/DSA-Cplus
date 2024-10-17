94d974e5826ef6ffa6c2d2b97539b0bbb606bd11
mramansingh21 <155548204+mramansingh21@users.noreply.github.com>
2024-10-18 04:08:35 +0530

    aman

---
+++ ifelse.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int number;
+    cin>> number;
+
+    if (number>0)
+    {
+        cout<< "number is positive" <<endl;
+    }
+    else
+       {
+         cout<< "number is negative" << endl;
+
+       }
+
+}
