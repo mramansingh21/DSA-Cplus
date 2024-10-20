20ae1d87c6255ce374a2ac8f4c4fa4737d3f84b7
mramansingh21 <155548204+mramansingh21@users.noreply.github.com>
2024-10-21 01:04:17 +0530

    Aman

---
+++ positive.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int a=9;
+
+   if (a==9)
+   {
+    cout << "NINEY";
+   }
+
+    if (a>0)
+   {
+     cout << "A is positive";
+    
+   }
+   else
+   {
+     cout << "A is negative";
+   }
+
+}
